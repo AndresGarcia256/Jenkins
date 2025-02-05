3d385ee065db29c12b30aa96fdf9c034576b475e
Andres garcia <98360920+AndresGarcia256@users.noreply.github.com>
2025-02-04 21:35:31 -0500

    Create enviar_correo.py

---
+++ enviar_correo.py
@@ -0,0 +1,18 @@
+import smtplib
+from email.message import EmailMessage
+
+remitente = "jgarcia77214@universidadean.edu.co"
+destinatario = "jgarcia77214@universidadean.edu.co"
+mensaje = "Hola mundo "
+email = EmailMessage()
+email["From"] = remitente
+email["To"] = destinatario  
+email["Subject"] = "Correo de prueba"
+email.set_content(mensaje)
+
+smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
+smtp.starttls()
+smtp.login(remitente, "SSTTQthgbh124*")
+smtp.sendmail(remitente, destinatario, email.as_string())
+smtp.quit()
+print("correo enviado")
